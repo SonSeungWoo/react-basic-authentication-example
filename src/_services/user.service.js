af06ac5d2f2a263c59d9c097358fdd175e54a687
sonseungwoo <skyghy3342@naver.com>
2019-07-07 14:44:49 +0900

    Initial commit

---
+++ src/_services/user.service.js
@@ -0,0 +1,58 @@
+import { authHeader } from '../_helpers';
+
+export const userService = {
+    login,
+    logout,
+    getAll
+};
+
+//여기서 통신
+function login(username, password) {
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
+    };
+
+    return fetch(`/users/authenticate`, requestOptions)
+        .then(handleResponse)
+        .then(user => {
+            if (user) {
+                user.authdata = window.btoa(username + ':' + password);
+                console.log('username', username)
+                localStorage.setItem('user', JSON.stringify(user));
+            }
+
+            return user;
+        });
+}
+
+function logout() {
+    localStorage.removeItem('user');
+}
+
+function getAll() {
+    const requestOptions = {
+        method: 'GET',
+        headers: authHeader()
+    };
+
+    return fetch(`/users`, requestOptions).then(handleResponse);
+}
+
+function handleResponse(response) {
+    return response.text().then(text => {
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                logout();
+                location.reload(true);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+
+        return data;
+    });
+}
